a90a01f9c4ed611d8808c2e90654ded9dabca7f4
bahagit <baha@cybertekschool.com>
2021-04-23 00:48:26 +0100

    Spring constructor injection

---
+++ Spring-00-LooselyCoupled/src/CybertekApp.java
@@ -0,0 +1,24 @@
+
+
+import service.FullTimeMentor;
+import service.MentorAccount;
+import service.PartTimeMentor;
+
+
+public class CybertekApp {
+    public static void main(String[] args) {
+
+        FullTimeMentor fullTime = new FullTimeMentor();
+        PartTimeMentor partTime = new PartTimeMentor();
+
+
+
+        MentorAccount mentor = new MentorAccount(fullTime);
+        mentor.manageAccount();
+
+        MentorAccount mentor2 = new MentorAccount(partTime);
+        partTime.createAccount();
+
+
+    }
+}
